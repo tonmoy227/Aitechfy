71fce523e224f4b9090b2da3a42924ba2a0fe806
tonmoy227 <145573783+tonmoy227@users.noreply.github.com>
2025-08-03 14:00:01 +0600

    70%
    70%

--- assets/js/script.js
+++ assets/js/script.js
@@ -107,12 +107,28 @@ Last change:    00/00/00
 		preloader: false,
 		fixedContentPos: false,
 	});
-
+	$('.marquee-left').marquee({
+		gap: 0,
+		speed: 40,
+		delayBeforeStart: 0,
+		direction: 'left',
+		duplicated: true,
+		pauseOnHover: true,
+		startVisible:true,
+	});
+	$('.marquee-right').marquee({
+		gap: 28,
+		speed: 40,
+		delayBeforeStart: 0,
+		direction: 'right',
+		duplicated: true,
+		pauseOnHover: true,
+		startVisible:true,
+	});
 	// windows-loaded-before-functions
 	document.addEventListener("DOMContentLoaded", function () {
 		window.addEventListener('load', function(){
 
-			CustomEase.create("ease1", ".645,.045,.355,1");
 
 			let preloader = document.querySelector("#preloader");
 			if (preloader) {
@@ -176,11 +192,6 @@ Last change:    00/00/00
 		slidesPerView: 5,
 		speed: 1000,
 		direction: 'vertical',
-		// autoplay: {
-		// 	enabled: true,
-		// 	delay: 6000
-		// },
-
 		navigation: {
 			prevEl: ".at-ser1-prev",
 			nextEl: ".at-ser1-next",
@@ -190,10 +201,19 @@ Last change:    00/00/00
 				slidesPerView: 5,
 			},
 			'1200': {
-				slidesPerView: 3,
+				slidesPerView: 5,
 			},
-			'1024': {
+			'1199': {
 				slidesPerView: 3,
+				direction: 'horizontal',
+			},
+			'480': {
+				slidesPerView: 2,
+				direction: 'horizontal',
+			},
+			'0': {
+				slidesPerView: 2,
+				direction: 'horizontal',
 			},
 		},
 	});
@@ -204,10 +224,6 @@ Last change:    00/00/00
 		speed: 1000,
 		slidesPerView: 1,
 		effect: "fade",
-		// autoplay: {
-		// 	enabled: true,
-		// 	delay: 6000
-		// },
 		navigation: {
 			prevEl: ".at-ser1-prev",
 			nextEl: ".at-ser1-next",
@@ -217,6 +233,87 @@ Last change:    00/00/00
 		},
 	});
 
+	if (window.matchMedia("(min-width: 1200px)").matches) { 
+		var ATTEAM = gsap.timeline({
+			scrollTrigger: {
+				trigger: '.at-tm1-sec',
+				start: "top 0%",
+				end: "top -100%",
+				scrub: 1,
+				pin: true,
+				pinSpacing: true,
+				markers: false,
+			}
 
+		});
+		ATTEAM
+		.from( ".at-tm1-content .at-tm1-bgclr" , { width: 0,  duration: 1, ease: "power2.out"})
+		.from( ".at-tm1-item-wrap" , { yPercent: 100, duration: 1, ease: "power2.out"},"<= .1")
+		.from( ".at-tm1-item:nth-child(1)" , { yPercent: 100, duration: 1, ease: "power2.out"},"<= .4")
+		.from( ".at-tm1-item:nth-child(4)" , { yPercent: 100, duration: 1, ease: "power2.out"},"<")
+	};
 
+	if (window.matchMedia("(min-width: 1200px)").matches) { 
+		var ATTEAM = gsap.timeline({
+			scrollTrigger: {
+				trigger: '.at-case-sec',
+				start: "top 80%",
+				end: "bottom 100%",
+				scrub: 1,
+				markers: false,
+			}
+
+		});
+		ATTEAM
+		.from( ".at-case-title h3" , {  color: "#6C1AFC" ,  letterSpacing: "200px", y: 500,  duration: 1, ease: "power2.out"})
+	};
+
+	const all_btns = gsap.utils.toArray(".bi-btn-hover");
+	const all_btn_circles = gsap.utils.toArray(".bi-btn-item");
+
+	if (all_btns.length > 0) {
+		all_btns.forEach((btn, i) => {
+			$(btn).mousemove(function (e) {
+				const rect = btn.getBoundingClientRect();
+				const relX = e.clientX - rect.left;
+				const relY = e.clientY - rect.top;
+
+				gsap.to(all_btn_circles[i], {
+					duration: 0.5,
+					x: relX - rect.width / 2,
+					y: relY - rect.height / 2,
+					ease: "power2.out",
+				});
+			});
+
+			$(btn).mouseleave(function () {
+				gsap.to(all_btn_circles[i], {
+					duration: 0.5,
+					x: 0,
+					y: 0,
+					ease: "power2.out",
+				});
+			});
+		});
+	}
+
+	if (window.matchMedia("(min-width: 1200px)").matches) { 
+		var ATWORKPROCESS = gsap.timeline({
+			scrollTrigger: {
+				trigger: '.at-step1-sec',
+				start: "top 0%",
+				end: "top -100%",
+				scrub: 1,
+				pin: true,
+				pinSpacing: true,
+				markers: false,
+			}
+
+		});
+		ATWORKPROCESS
+		.to( ".at_step_type_1" , { rotate: "18deg",  y: -120,   duration: 1, ease: "power2.out"})
+		.to( ".at_step_type_2" , { rotate: "-18deg",  y: -640,   duration: 1, ease: "power2.out"},"<=.15")
+		.to( ".at_step_type_3" , { rotate: "6deg",  y: -620,   duration: 1, ease: "power2.out"},"<=.6")
+		.to( ".at_step_type_4" , { rotate: "0",  y: -610,   duration: 1, ease: "power2.out"},"<=.6")
+	};
 })(jQuery);
